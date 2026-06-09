7fd631f6d84eb1b915d4128d742958ef30a5a920
Peter Polidoro <peter@polidoro.io>
2026-06-09 16:00:42 -0400

    Update guix files for kicad 10

--- .metadata/guix/channels.scm
+++ .metadata/guix/channels.scm
@@ -1,17 +1,11 @@
 ;; This file is generated automatically from metadata
 ;; File edits may be overwritten!
 (list (channel
-        (name 'guix-janelia)
-        (url "https://github.com/guix-janelia/guix-janelia.git")
-        (branch "main")
-        (commit
-          "6d2cb9cdd679f90c1a4fb02af02d2d1cfe2fcca6"))
-      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "3b5533f9bdac4a929c3d93e628ee2ae279013685")
+          "47dffdc2f6e2166bb2c17d235e350abcbb31182a")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
